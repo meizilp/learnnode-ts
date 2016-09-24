50c1ede03fb2f52fe6cfaf1aa8e8d95b852d2d7a
meizilp <meizilp@gmail.com>
2016-09-24 21:03:35 +0800

    add router

--- 02/app.ts
+++ 02/app.ts
@@ -1,12 +1,38 @@
 import * as express from 'express'
+import * as bodyParser from 'body-parser'
 
 let app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');    
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let router = express.Router();
+
+router.use((req, res, next) => {
+    console.log('something is happending.');
+    next();
 });
+
+router.get('/', (req, res) => {
+    res.status(200).json({ msg: 'Welcome use my API!' });
+})
+
+router.route('/bears')
+    .post((req, res) => {
+        res.status(201).json({ msg: 'Bear created!' })
+    })
+    .get((req, res) => {
+        console.log(`${__dirname}/mock/user1.json`)
+        res.status(200).sendFile(`${__dirname}/mock/user1.json`)
+    })
+
+router.route('/bears/:id')
+    .get((req, res) => {
+        res.status(200).json({ _id: req.params.id })
+    })
+
+app.use('/api', router);
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
-
